638651d80c0d1475e29a7f0ef943b89b03595772
OtacilioN <otacilio.n97@gmail.com>
2018-10-05 18:54:12 -0300

    another try

--- osmn.c
+++ osmn.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
@@ -6,93 +7,90 @@ int main(void)
     while (1)
     {
         scanf("%d", &interactions);
-        int priority[interactions], level[interactions], lowest = 1;
+        int priority[interactions], level[interactions], maxNumber = 0, maxN, nIndex;
         int lastParent = -1, opened = 0;
-        char label[interactions];
+        char label[interactions][105];
         if (interactions == 0)
             return 0;
 
-        scanf(" %c/%d", &label[0], &priority[0]);
-        level[0] = 1;
-
-        for (x = 1; x < interactions; x++)
+        for (x = 0; x < interactions; x++)
         {
-            scanf(" %c/%d", &label[x], &priority[x]);
-            if (priority[x] > priority[x - 1])
-            {
-                if (lastParent > 0)
-                {
-                    level[x] = level[x - 1] - 1 - lastParent;
-                    // lastParent--;
-                }
-
-                else
-                {
-                    level[x] = level[x - 1] - 1;
-                    if (lastParent == -1)
-                    {
-                        lastParent = 0;
-                    }
-                    else
-                    {
-                        lastParent = -2;
-                    }
-                }
-            }
-            else
-            {
-                if (lastParent > 0)
-                {
-                    level[x] = level[x - 1] + 1 + lastParent;
-                    lastParent--;
-                }
-                else
-                {
-                    level[x] = level[x - 1] + 1;
-
-                    if (lastParent == 0)
-                    {
-                        lastParent++;
-                    }
-                    else
-                    {
-                        lastParent = -1;
-                    }
-                }
-            }
-            if (level[x] < lowest)
-                lowest = level[x];
+            scanf(" %[a-z]/%d", label[x], &priority[x]);
+            for (nIndex = 1; pow(2, nIndex) < (((interactions - priority[x])) + 2); nIndex++)
+                ;
+            level[x] = nIndex;
         }
-        for (x = 0; x < level[0] + 1 - lowest; x++)
+
+        for (x = 0; x < level[0]; x++)
         {
             printf("(");
+            opened++;
         }
-        printf("%c/%d", label[0], priority[0]);
+        printf("%s/%d", label[0], priority[0]);
+
         for (x = 1; x < interactions; x++)
         {
             if (level[x] > level[x - 1])
             {
                 for (y = level[x - 1]; y < level[x]; y++)
+                {
                     printf("(");
+                    opened++;
+                }
+            }
+            else if (level[x] < level[x - 1])
+            {
+                for (y = level[x]; y < level[x - 1]; y++)
+                {
+                    printf(")");
+                    opened--;
+                }
             }
             else
             {
-                for (y = level[x]; y < level[x - 1]; y++)
-                    printf(")");
+                printf("(");
+                opened++;
             }
-            printf("%c/%d", label[x], priority[x]);
+
+            printf("%s/%d", label[x], priority[x]);
         }
-        if (lastParent > 0)
-            level[interactions - 1]++;
-        for (x = 0; x < level[interactions - 1] + 1 - lowest; x++)
+        for (x = 0; x < opened; x++)
         {
             printf(")");
         }
-        printf("\n\n");
+        printf("\n");
+
+        // for (x = 0; x < level[0]; x++)
+        // {
+        //     printf("(");
+        // }
+        // printf("%s/%d", label[0], priority[0]);
+        // for (x = 1; x < interactions; x++)
+        // {
+        //     if (level[x] > level[x - 1])
+        //     {
+        //         for (y = level[x - 1]; y < level[x]; y++)
+        //             printf("(");
+        //     }
+        //     else
+        //     {
+        //         for (y = level[x]; y < level[x - 1]; y++)
+        //             printf(")");
+        //     }
+        //     printf("%s/%d", label[x], priority[x]);
+        // }
+        // if (lastParent > 0)
+        //     level[interactions - 1]++;
+        // for (x = 0; x < level[interactions - 1]; x++)
+        // {
+        //     printf(")");
+        // }
+        // printf("\n");
         // for (x = 0; x < interactions; x++)
         // {
-        //     printf(" %d ", level[x] + 1 - lowest);
+        //     printf("(%d|%d) ", priority[x], level[x]);
         // }
+        // printf("\n");
     }
     printf("OTA \n");
     return 0;
